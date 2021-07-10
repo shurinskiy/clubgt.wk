4906603f2d8d8731baf42410f83383da48d59416
_tango <shurinskiy@mail.ru>
2021-07-10 21:51:57 +0600

    add zoom to showcase

--- src/blocks/events/events.js
+++ src/blocks/events/events.js
@@ -4,11 +4,6 @@
 
 	if($items.length > 4) {
 		$('.events__items').after("<span class='events__more'>all events</span>");
-
-		$items.each(function(index) {
-			if(index > 3)
-				$(this).hide();
-		});
 	}
 
 	$('.events').on('click', '.events__more', function() {
--- src/blocks/showcase/showcase.js
+++ src/blocks/showcase/showcase.js
@@ -1,6 +1,9 @@
 import lightGallery from 'lightgallery';
+import lgZoom from 'lightgallery/plugins/zoom'
 
 (() => {
-	lightGallery(document.getElementById('showcase__items'), {});
+	lightGallery(document.getElementById('showcase__items'), {
+		plugins: [lgZoom]
+	});
 
 })();
